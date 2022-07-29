6b409da4df49af9078229e43f03083f923848ad9
SeanRichhh <sean-richards@outlook.com>
2022-07-29 23:53:28 +0100

    Second requirement passed with test

--- spec/TodoList.spec.js
+++ spec/TodoList.spec.js
@@ -18,12 +18,58 @@ describe("TodoList", () => {
     // set up
     const todoList = new TodoList()
     const expected = {
-      id: 2,
-      text: "turn the heating off!",
+      id: 1,
+      text: "turn the aircon down!",
       status: "incomplete"
     }
     // execute
-    const result = todoList.create("turn the heating off!")
+    const result = todoList.create("turn the aircon down!")
+    // verify
+    expect(result).toEqual(expected)
+  })
+  
+  /// Second Test with the ID
+  
+  it("should increment the id", () => {
+    // set up
+    const todoList = new TodoList();
+    const firstTodo = {
+      id: 1,
+      text: "turn the heating on!",
+      status: "incomplete"
+    }
+    const secondTodo = {
+      id: 2,
+      text: "turn the aircon down!",
+      status: "incomplete"
+    }
+
+    // execute
+    todoList.create("turn the heating on!")
+    const result = todoList.create("turn the aircon down!")
+
+    expect(result).toEqual(secondTodo)
+  })
+
+  // Gets all todo items
+
+  it("Get all todo items", () => {
+    const todoList = new TodoList();
+    // set up
+    const expected = [{
+      id: 1,
+      text: 'turn the heating on!',
+      status: 'incomplete'
+    },
+    {
+      id: 2,
+      text: 'turn the aircon down!',
+      status: 'incomplete'
+    }]
+    // execute
+    todoList.create('turn the heating on!')
+    todoList.create('turn the aircon down!')
+    const result = todoList.getAllToDo()
     // verify
     expect(result).toEqual(expected)
   })
--- src/TodoList.js
+++ src/TodoList.js
@@ -1,19 +1,24 @@
 class TodoList {
     todoList = []
  
-
+    // Create a todo item that has an ID, text and starts off incomplete
     create(text){
         const todoListCreate = {
             id: this.todoList.length + 1,
             text: text,
             status: "incomplete"
         } 
-        this.todoList.push(todoListCreate);
+        this.todoList.push(todoListCreate)
 
         return todoListCreate
     }
 
+    // Get all the todo Items
+    getAllToDo(){
+        return this.todoList
+    }
 
+    //Set a todo completed by its ID
 }
 
 module.exports = TodoList
